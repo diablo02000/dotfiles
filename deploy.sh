ea67bfb0c6eab3c218ade5662e49248287d342b8
diablo02000 <christophe.biguereau@gmail.com>
2025-12-02 18:38:25 +0100

    fix(shell): shell format

--- deploy.sh
+++ deploy.sh
@@ -8,43 +8,41 @@ declare -r DOTFILES_VIM_CONFIG_DIR="${HOME}/.vim"
 declare -r DOTFILES_VIM_AUTOLOAD_DIR="${DOTFILES_VIM_CONFIG_DIR}/autoload"
 
 # Output message to user console.
-function output(){
-  local message="${1}"
-  printf "%b\n" "${message}"
+function output() {
+	local message="${1}"
+	printf "%b\n" "${message}"
 }
 
 # Load .env file if exists.
-if [[ -f .env ]];
-then
-  # Export variable from env file.
-  export "$(xargs < .env)"
+if [[ -f .env ]]; then
+	# Export variable from env file.
+	export "$(xargs <.env)"
 else
-  # Validate path to ssh key
-  read -rp "What is the full Path to your SSH key? [${DOTFILES_SSH_KEY_FULL_PATH}] "  user_answer
-  if [[ ${user_answer} != "" ]];
-  then
-    readonly DOTFILES_SSH_KEY_FULL_PATH="${user_answer}"
-  fi
+	# Validate path to ssh key
+	read -rp "What is the full Path to your SSH key? [${DOTFILES_SSH_KEY_FULL_PATH}] " user_answer
+	if [[ ${user_answer} != "" ]]; then
+		readonly DOTFILES_SSH_KEY_FULL_PATH="${user_answer}"
+	fi
 
-  # Save user answer to env file
-  readarray -t shell_var < <(set -o posix ; set)
-  for var in "${shell_var[@]}";
-  do
-    if [[ ${var} =~ 'DOTFILES_' ]];
-    then
-      echo "DOTFILES_SSH_KEY_FULL_PATH='${DOTFILES_SSH_KEY_FULL_PATH}'" > .env
-    fi
-  done
+	# Save user answer to env file
+	readarray -t shell_var < <(
+		set -o posix
+		set
+	)
+	for var in "${shell_var[@]}"; do
+		if [[ ${var} =~ 'DOTFILES_' ]]; then
+			echo "DOTFILES_SSH_KEY_FULL_PATH='${DOTFILES_SSH_KEY_FULL_PATH}'" >.env
+		fi
+	done
 fi
 
 ###
 # Configure Vim
 ###
 # Install vim-plug (Required for plugin installation)
-if [[ ! -f "${DOTFILES_VIM_AUTOLOAD_DIR}/plug.vim" ]];
-then
-  output "Install vim-plug."
-  curl -fLo "${DOTFILES_VIM_AUTOLOAD_DIR}/plug.vim" --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+if [[ ! -f "${DOTFILES_VIM_AUTOLOAD_DIR}/plug.vim" ]]; then
+	output "Install vim-plug."
+	curl -fLo "${DOTFILES_VIM_AUTOLOAD_DIR}/plug.vim" --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 fi
 
 # Deploy vim user configuration
@@ -56,10 +54,9 @@ cp -u .vim/* "${DOTFILES_VIM_CONFIG_DIR}/"
 vim_plugins_file_timestamp="$(date -r "${DOTFILES_VIM_CONFIG_DIR}/plugins.vim" +%s)"
 modify_timestamp_limit="$(date -d 'now - 2 minutes' +%s)"
 
-if [[ $vim_plugins_file_timestamp -ge $modify_timestamp_limit ]];
-then
-  output "Install vim plugins."
-  vim +PlugInstall +qall
+if [[ $vim_plugins_file_timestamp -ge $modify_timestamp_limit ]]; then
+	output "Install vim plugins."
+	vim +PlugInstall +qall
 fi
 
 ###
@@ -81,21 +78,20 @@ cp -ru .curl "${HOME}/.curl"
 ###
 output "Deploy custom shell configuration."
 cp -ru .functions "${HOME}/"
-envsubst < .aliases > "${HOME}/.aliases"
+envsubst <.aliases >"${HOME}/.aliases"
 
 # Load specific file for Mac OS
-if [[ $OSTYPE == 'darwin'* ]];
-then
-    cp .bindkeys_macos "${HOME}/.bindkeys_macos"
+if [[ $OSTYPE == 'darwin'* ]]; then
+	cp .bindkeys_macos "${HOME}/.bindkeys_macos"
 fi
 
 # add shell custom file base on shell
 case ${SHELL} in
-  *zsh)
-    cp .zshrc_custom "${HOME}/.zshrc_custom"
-    output "To enable zshrc_custom file to need to add the following command in your ~/.zshrc:\n\tsource ${HOME}/.zshrc_custom"
-    ;;
-  *)
-    output "Shell ${SHELL} not handle yet."
-    ;;
+*zsh)
+	cp .zshrc_custom "${HOME}/.zshrc_custom"
+	output "To enable zshrc_custom file to need to add the following command in your ~/.zshrc:\n\tsource ${HOME}/.zshrc_custom"
+	;;
+*)
+	output "Shell ${SHELL} not handle yet."
+	;;
 esac
--- oh-my-sh/custom/plugins/workspace-dir/workspace-dir.plugin.zsh
+++ oh-my-sh/custom/plugins/workspace-dir/workspace-dir.plugin.zsh
@@ -1,43 +1,46 @@
+# shellcheck disable=SC2148
 # Configuration variables
 # Define workspace base directory
 DEFAULT_WORKSPACE_ROOT_DIR="${HOME}/workspaces"
-: ${WORKSPACE_ROOT_DIR:=$DEFAULT_WORKSPACE_ROOT_DIR}
+: "${WORKSPACE_ROOT_DIR:=$DEFAULT_WORKSPACE_ROOT_DIR}"
 
 # Functions
 # Create daily working directory.
 function createWorkingSpace() {
-  local WORKSPACE="${WORKSPACE_ROOT_DIR}/$(date +%F)"
-  [[ ! -d "${WORKSPACE}" ]] && mkdir -p "${WORKSPACE}"
-  cd "${WORKSPACE}"
+	CURRENT_DATE="$(date +%F)"
+	readonly CURRENT_DATE
+	local WORKSPACE="${WORKSPACE_ROOT_DIR}/${CURRENT_DATE}"
+	[[ ! -d "${WORKSPACE}" ]] && mkdir -p "${WORKSPACE}"
+	cd "${WORKSPACE}" || exit
 }
 
-function changeDirectoryToWorkspaceDir(){
-  # one char = 1 day
-  local days_before="${1}"
+function changeDirectoryToWorkspaceDir() {
+	# one char = 1 day
+	local days_before="${1}"
 
-  if [[ -z "${days_before}" ]];
-  then
-    # Create new directory
-    createWorkingSpace
-  else
-    # Get parameters length
-    nbr_days="${#days_before}"
-    cd "${WORKSPACE_ROOT_DIR}/$(date -d "-${nbr_days} day" +%F)"
-  fi
+	if [[ -z "${days_before}" ]]; then
+		# Create new directory
+		createWorkingSpace
+	else
+		# Get parameters length
+		nbr_days="${#days_before}"
+		cd "${WORKSPACE_ROOT_DIR}/$(date -d "-${nbr_days} day" +%F)" || exit
+	fi
 }
 
-function selectWorkspaceDir(){
-  local formatted_date=""
+function selectWorkspaceDir() {
+	local formatted_date=""
 
-  # List folder in Workspace dir
-  for i in $(find ${DEFAULT_WORKSPACE_ROOT_DIR} -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
-  do
-    # Convert date to full date
-    formatted_date+="$(date -d"${i}" +%A" "%d" "%B)\n"
-  done
+	# List folder in Workspace dir
+	# shellcheck disable=SC2044
+	for i in $(find "${DEFAULT_WORKSPACE_ROOT_DIR}" -mindepth 1 -maxdepth 1 -type d -exec basename {} \;); do
+		# Convert date to full date
+		# shellcheck disable=SC2140
+		formatted_date+="$(date -d"${i}" +%A" "%d" "%B)\n"
+	done
 
-  folder="$(echo -e "${formatted_date}" | fzf +m)"
-  cd "${DEFAULT_WORKSPACE_ROOT_DIR}/$(date -d"${folder}" +%F)"
+	folder="$(echo -e "${formatted_date}" | fzf +m)"
+	cd "${DEFAULT_WORKSPACE_ROOT_DIR}/$(date -d"${folder}" +%F)" || exit
 }
 
 # Aliases
